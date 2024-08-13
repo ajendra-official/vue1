e1d51d04f6d3ea352248d547b4fc57a38bade0ce
ajendra singh yadav <ajendra.yadav@mastek.com>
2024-08-13 11:23:13 +0530

    First push

--- src/pages/coaches/CoachDetail.vue
+++ src/pages/coaches/CoachDetail.vue
@@ -8,7 +8,7 @@
   <section>
     <base-card>
       <header>
-        <h2>Interested? Reach out now!</h2>
+        <h2>Interested? Reach out now!!!</h2>
         <base-button link :to="contactLink">Contact</base-button>
       </header>
       <router-view></router-view>
